35b11216043a860d6b258093fdc8883c87dd0b26
GuiRL-Dev <guirl.dev@gmail.com>
2025-05-29 21:10:45 -0300

    Subindo projeto atual

---
+++ src/main/java/com/rldev/login_auth_api/domain/user/User.java
@@ -0,0 +1,22 @@
+﻿package com.rldev.login_auth_api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "users")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+}
